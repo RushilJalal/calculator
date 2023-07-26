428641da7f80325d043d8fe2c8a1995292bf18c3
Rushil Jalal <rushiljalal@gmail.com>
2023-07-27 01:03:47 +0530

    Add math operator functions

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNum;
+let secondNum;
+let operator;
+
+function operate(firstNum, secondNum, operator) {
+    switch (operator) {
+        case '+':
+            return add(firstNum, secondNum)
+        case '-':
+            return subtract(firstNum, secondNum)
+        case '*':
+            return multiply(firstNum, secondNum)
+        case '/':
+            return divide(firstNum, secondNum)
+        default:
+            console.log('Invalid operator');
+            break;
+    }
+}
+const screen = document.querySelector(".screen")
+screen.textContent = operate(5, 7, '-')
