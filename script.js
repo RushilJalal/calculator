1103754a04243b25e5b955d44f28b7fefb04f225
Rushil Jalal <rushiljalal@gmail.com>
2023-08-06 13:13:49 +0530

    Add single operator calculations

--- script.js
+++ script.js
@@ -33,5 +33,27 @@ function operate(firstNum, secondNum, operator) {
             break;
     }
 }
+const operators = ['+', '-', '*', '/']
 const screen = document.querySelector(".screen")
-screen.textContent = operate(5, 7, '-')
+let screenContent, first, second;
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        screen.textContent += e.currentTarget.value;
+        screenContent = screen.textContent;
+        operators.includes(e.currentTarget.id) ? operator = e.currentTarget.id : '';
+        console.log(operator);
+        if (e.currentTarget.value === '=') {
+            parts = screenContent.split(/[+\-*/=]/)
+            console.log(parts);
+            first = Number(parts[0]);
+            second = Number(parts[1]);
+            screen.textContent = operate(first, second, operator)
+        }
+    })
+})
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    screen.textContent = '';
+})
