6ea20cb25799763a9a0f2f02a6c1c9dbab80c67f
Rushil Jalal <rushiljalal@gmail.com>
2023-08-08 20:29:17 +0530

    Add only 1 operator at a time

--- script.js
+++ script.js
@@ -39,27 +39,44 @@ const screen = document.querySelector(".screen")
 let screenContent, first, second;
 const buttons = document.querySelectorAll('.btn');
 buttons.forEach(button => {
-    button.addEventListener("click", (e) => {
+    button.addEventListener("click", press)
+})
+window.addEventListener('keydown', (num) => {
+    permissible = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Enter', '+', '-', '*', '/'];
+    console.log(num.key);
+    if (permissible.includes(num.key)) {
+        key = document.querySelector(`button[data-key="${num.key}"]`);
+        key.click();
+    }
+
+    if (num.key === 'Backspace') {
+        remove();
+    }
+});
+
+
+function press(e) {
+    if (!(operators.includes(e.currentTarget.id) && operators.includes(screen.textContent.at(-1)))) {
         screen.textContent += e.currentTarget.value;
         screenContent = screen.textContent;
         operators.includes(e.currentTarget.id) ? operator = e.currentTarget.id : '';
-        console.log(operator);
-        if (e.currentTarget.value === '=') {
-            parts = screenContent.split(/[+\-*/=]/)
-            console.log(parts);
-            first = Number(parts[0]);
-            second = Number(parts[1]);
-            screen.textContent = operate(first, second, operator)
-        }
-    })
-})
+    }
 
+    if (e.currentTarget.value === '=') {
+        parts = screenContent.split(/[+\-*/=]/)
+        first = Number(parts[0]);
+        second = Number(parts[1]);
+        screen.textContent = operate(first, second, operator)
+    }
+}
 const clear = document.querySelector("#clear");
 clear.addEventListener("click", () => {
     screen.textContent = '';
 })
 
 const del = document.querySelector("#delete");
-del.addEventListener("click", () => {
-    screen.textContent = screenContent.slice(0, -1);
-})
+del.addEventListener("click", remove)
+
+function remove() {
+    screen.textContent = screen.textContent.slice(0, -1);
+}
