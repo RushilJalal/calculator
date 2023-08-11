8b17701c4850851f0ad86733fe0eee0000367a74
Rushil Jalal <rushiljalal@gmail.com>
2023-08-11 23:47:43 +0530

    Change calc logic

--- script.js
+++ script.js
@@ -43,7 +43,6 @@ buttons.forEach(button => {
 })
 window.addEventListener('keydown', (num) => {
     permissible = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'Enter', '+', '-', '*', '/'];
-    console.log(num.key);
     if (permissible.includes(num.key)) {
         key = document.querySelector(`button[data-key="${num.key}"]`);
         key.click();
@@ -56,7 +55,7 @@ window.addEventListener('keydown', (num) => {
 
 
 function press(e) {
-    if (!(operators.includes(e.currentTarget.id) && operators.includes(screen.textContent.at(-1)))) {
+    if (!(operators.includes(e.currentTarget.id) && operators.includes(screen.textContent.at(-1)))) {//can't click two operators together
         screen.textContent += e.currentTarget.value;
         screenContent = screen.textContent;
         operators.includes(e.currentTarget.id) ? operator = e.currentTarget.id : '';
@@ -64,6 +63,7 @@ function press(e) {
 
     if (e.currentTarget.value === '=') {
         parts = screenContent.split(/[+\-*/=]/)
+        console.log(parts);
         first = Number(parts[0]);
         second = Number(parts[1]);
         screen.textContent = operate(first, second, operator)
